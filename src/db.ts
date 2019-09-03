e14e154a9c7d13984724a1f36bac0d2fccd1e439
Dawid Szafrański <dawid.szafranski@fingoweb.com>
2019-09-03 11:27:53 +0200

    fix deploy

--- src/db.ts
+++ src/db.ts
@@ -1,6 +1,8 @@
 import * as knexfile from '../knexfile.js';
 import * as Knex from 'knex';
 
-const knex = Knex(knexfile.staging);
+const ENVIROMENT = process.env.ENV || "development";
+
+const knex = Knex(knexfile[ENVIROMENT]);
 
 export { knex, Knex as IKnex };
