f17f182760c3c73e2b1f1363816c5356310db756
Dawid Szafrański <dawid.szafranski@fingoweb.com>
2019-09-03 11:31:04 +0200

    Updates

--- db.js
+++ db.js
@@ -3,6 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var knexfile = require("../knexfile.js");
 var Knex = require("knex");
 exports.IKnex = Knex;
-var knex = Knex(knexfile.staging);
+var ENVIROMENT = process.env.ENV || "development";
+var knex = Knex(knexfile[ENVIROMENT]);
 exports.knex = knex;
 //# sourceMappingURL=db.js.map
